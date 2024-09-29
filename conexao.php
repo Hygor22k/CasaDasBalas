10cf15062e57801aab057f2d8f4d927e49304ee1
Hygor Macedo de Minas <hygormacedo22k@gmail.com>
2024-09-29 20:24:25 -0300

    Conexão-BD

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php 
+
+    define("HOST","localhost");
+    define("USER","root");
+    define("PASS","");
+    define("BASE","casa-das-balas-04");
+
+    $conexao = new mysqli(HOST,USER,PASS,BASE);
